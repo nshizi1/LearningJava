15002896de4e249f4cdd34b6e5cb76a60bb3d110
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2023-10-30 13:26:01 +0200

    println

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is wilson");
+        System.out.println("And I am 17 years old");
+        // this is a comment
+        System.out.println(5*6);
+    }
+}
