879c7dfc77ec36aeeac135b0d004db2d7e187e6c
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2023-10-30 16:31:19 +0200

    type casting

--- App.java
+++ App.java
@@ -4,19 +4,45 @@ public class App {
         System.out.println("Hello World");
         System.out.println("And I am 17 years old");*/
         // this is a comment
-        System.out.println(5*6);
+        // System.out.println(5*6);
 
 
 
         // variables
 
-        String name = "Wilson";
-        System.out.println(name);
-        int age = 17;
-        System.out.println(age);
+        // String name = "Wilson";
+        // System.out.println(name);
+        // int age = 17;
+        // System.out.println(age);
         
         final int num = 5;
 
         System.out.println(num);
+
+        String myName = "Wilson Nshizirungu";
+        String englishWord ="I am";
+        int myAge = 17;
+        System.out.println(myName+" "+englishWord+" "+myAge);
+
+        String $car = "Lamborghini";
+
+        String _color = "Dark blue";
+
+        System.out.println("I have a "+ $car+ " car, and it is of a "+_color+" color");
+
+
+        // type casting
+
+        // int myNumber = 9;
+        // float myFloat = myNumber;
+        // System.out.println(myFloat);
+
+        // double myDouble = 9.89d;
+        // float myFloat = (float) myDouble;
+        // System.out.println(myFloat);
+        // int myInt = (int) myFloat;
+        // System.out.println(myInt);
+
+
     }
 }
