18d91d2e3032e95e64ad98c08e8a994541a1b631
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2023-10-30 14:01:58 +0200

    learning variables

--- App.java
+++ App.java
@@ -1,9 +1,22 @@
 public class App {
     public static void main(String[] args) {
+        /*System.out.println("This is wilson");
         System.out.println("Hello World");
-        System.out.println("This is wilson");
-        System.out.println("And I am 17 years old");
+        System.out.println("And I am 17 years old");*/
         // this is a comment
         System.out.println(5*6);
+
+
+
+        // variables
+
+        String name = "Wilson";
+        System.out.println(name);
+        int age = 17;
+        System.out.println(age);
+        
+        final int num = 5;
+
+        System.out.println(num);
     }
 }
